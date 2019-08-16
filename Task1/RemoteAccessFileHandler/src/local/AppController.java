87bf9905845c06367a63880df5e2ecd1812bdcde
Chang <newckdgns@naver.com>
2019-08-16 19:26:44 +0900

    Update user interface

--- Task1/RemoteAccessFileHandler/src/local/AppController.java
+++ Task1/RemoteAccessFileHandler/src/local/AppController.java
@@ -13,14 +13,14 @@ public class AppController {
     public static void main(String[] args) {
 
         scan = new Scanner(System.in);
-        System.out.println("/.......................................................\\");
-        System.out.println("|...........Start Remote Shell Access Program...........|");
-        System.out.println("\\......................................................./\n");
+        System.out.println("/.............................................................\\");
+        System.out.println("|..............Start Remote Shell Access Program..............|");
+        System.out.println("\\............................................................./\n");
 
         JRemoteSecureAccessor jrsa = new JRemoteSecureAccessor();
 
-        System.out.println("> Enter Target's SSH Access Info below...");
-        System.out.println("-----------------------------------------");
+        System.out.println(">>>>>>>>>>> Enter Target's SSH Access Info below...<<<<<<<<<<<");
+        System.out.println("--------------------------------------------------------------");
         System.out.print("  IP : ");//"192.168.1.126"
         jrsa.target.setTarget(scan.next());
         System.out.print("user : ");//ubuntu
@@ -29,7 +29,7 @@ public class AppController {
         jrsa.target.setPass(scan.next());
         System.out.print("port : ");//22
         jrsa.target.setPort(scan.nextInt());
-        System.out.println("-----------------------------------------");
+        System.out.println("--------------------------------------------------------------");
 
         jrsa.ConnectToRemoteComputer();
     }
